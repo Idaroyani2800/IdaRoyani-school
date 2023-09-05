bababe2865af7f17a7cb0694fee66813fa80ef68
Idaroyani2800 <144081860+Idaroyani2800@users.noreply.github.com>
2023-09-05 21:30:13 +0700

    Add files via upload

---
+++ PUBLIC/js/script.js
@@ -0,0 +1,38 @@
+$(function() {
+
+    $('.tombolTambahData').on('click',function(){
+        $('#staticBackdropLabel').html('Tambah Data Siswa');
+        $('.modal-footer button[type=submit]').html('Tambah Data');
+        $('#nama').val('');
+        $('#jenis_kelamin').val('');
+        $('#alamat').val('');
+        $('#id').val('');
+
+    });
+
+
+    $('.tampilModalUbah').on('click',function() {
+
+        $('.#staticBackdropLabel').html('Ubah Data Siswa'); 
+        $('.modal-footer button[type=submit]').html('Ubah Data'); 
+        $('.modal-body form').attr('action', 'http://localhost/phpmvc/PUBLIC/siswa/ubah');
+
+        const id = $(this).data('id');
+
+        $.ajax({
+            url:'http://localhost/phpmvc/PUBLIC/siswa/getubah',
+            data: {id : id},
+            method:'post',
+            dataType: 'json',
+            success:function(data){
+                $('#nama').val(data.nama);
+                $('#jenis_kelamin').val(data.jenis_kelamin);
+                $('#alamat').val(data.alamat);
+                $('#id').val(data.id);
+
+            }
+        });
+    });
+});
+
+
